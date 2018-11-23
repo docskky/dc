427a14d5250fadd89d216f62fa11b888aa55ac82
kiyong <kiyong@artpigsoft.co.kr>
2018-11-23 13:08:44 +0900

    auto update stocks

--- update_stock_codes.py
+++ update_stock_codes.py
@@ -17,7 +17,7 @@ import torch
 import lib.environ as environ
 import lib.pdenviron as pdenviron
 import lib.models as models
-import lib.data as data
+import lib.data
 
 
 """
@@ -25,7 +25,9 @@ import lib.data as data
 """
 stmt1 = "insert IGNORE into stocks (scode, name) VALUES (%s,%s);"
 stmt2 = "select max(date) from history_days where scode=%s;"
-stmt3 = "insert IGNORE into history_days (scode, date, open, high, low, close, volume) VALUES (%s,%s, %s, %s,%s,%s,%s);"
+stmt3 = "insert into history_days (scode, date, open, high, low, close, volume) VALUES (%s,%s, %s, %s,%s,%s,%s) \
+            ON DUPLICATE KEY UPDATE \
+            open=%s, high=%s, low=%s, close=%s, volume=%s;"
 stmt4 = "insert IGNORE into predicts (scode, date, predict_days, expect1, expect2, expect3, expect4, expect5) \
             VALUES (%s,%s,%s,%s,%s,%s,%s,%s);"
 date_format = '%Y-%m-%d'
@@ -50,8 +52,12 @@ def update_stocks(db, df, ext):
                     row = cur.fetchone()
                     last_date = row[0]
                     if last_date:
-                        if last_date == datetime.datetime.now().date():
-                            return
+                        if last_date >= datetime.datetime.now().date() - datetime.timedelta(days=1):
+                            # 저장 일자가 하루 전날 까지면 스킵한다.
+                            break
+                        # 마지막 저장일 7일 전 데이터 부터 검색을 시작한다.
+                        last_date - datetime.timedelta(days=7)
+                        from_date = last_date
                     else:
                         from_date = config.history_begin
 
@@ -64,7 +70,8 @@ def update_stocks(db, df, ext):
                         low = row['Low']
                         close = row['Close']
                         volume = row['Volume']
-                        cur.execute(stmt3, (scode, date, open, high, low, close, volume))
+                        cur.execute(stmt3, (scode, date, open, high, low, close, volume,
+                                            open, high, low, close, volume))
                         db.commit()
                     done = True
             except Exception as ex:
@@ -81,7 +88,7 @@ def update_prediction(db):
     predict_days = [7, 14, 30]
     nets = []
     # environment의 shape 샘플이 필요하므로 KQ003380 종목을 임의로 로드한다.
-    sample_prices_list, _valid_list = data.load_prices(["KQ003380"])
+    sample_prices_list, _valid_list = lib.data.load_prices(["KQ003380"])
     for pdays in predict_days:
         file_path = "data/v3.0-phase3-{}.data".format(pdays)
         env = pdenviron.PredEnv(prices_list=sample_prices_list, predict_days=pdays)
@@ -93,18 +100,26 @@ def update_prediction(db):
     today = datetime.datetime.now().date()
     with closing(db.cursor()) as cur:
         cur.execute("select scode from stocks")
-        for row in cur:
+        for row in cur.fetchall():
             scode = row[0]
-            prices_list, val_prices_list = data.load_prices([scode])
-            with closing(db.cursor()) as cur2:
-                for i in range(0, len(nets)):
-                    env = pdenviron.PredEnv(prices_list=prices_list, predict_days=pdays)
+            prices_list, val_prices_list = lib.data.load_prices([scode])
 
-                    # offset을 마지막 일자로 한다.
-                    obs = env.reset(0, len(prices_list[0].open)-1)
-                    values = environ.apply_model_from_state(obs, nets[i])
-                    # 예측결과 저장
-                    cur2.execute(stmt4, (scode, today, pdays, values[0], values[1], values[2], values[3], values[4]))
+            if len(prices_list[0].open) < 60:
+                continue
+
+            try:
+                with closing(db.cursor()) as cur2:
+                    for i in range(0, len(nets)):
+                        pdays = predict_days[i]
+                        env = pdenviron.PredEnv(prices_list=prices_list, predict_days=pdays)
+
+                        # offset을 마지막 일자로 한다.
+                        obs = env.reset(0, len(prices_list[0].open)-1)
+                        values = environ.apply_model_from_state(obs, nets[i])
+                        # 예측결과 저장
+                        cur2.execute(stmt4, (scode, today, pdays, values[0], values[1], values[2], values[3], values[4]))
+            except Exception as ex:
+                print(ex)
 
 
 if __name__ == "__main__":
@@ -112,10 +127,12 @@ if __name__ == "__main__":
     db = apdb()
     db.connect()
 
-    kosdaq = stockcode.download_stock_codes('kosdaq')
-    update_stocks(db, kosdaq, 'KQ')
+    #kosdaq = stockcode.download_stock_codes('kosdaq')
+    #update_stocks(db, kosdaq, 'KQ')
 
-    kospi = stockcode.download_stock_codes('kospi')
-    update_stocks(db, kosdaq, 'KS')
+    #kospi = stockcode.download_stock_codes('kospi')
+    #update_stocks(db, kospi, 'KS')
+
+    update_prediction(db)
 
     db.close()
